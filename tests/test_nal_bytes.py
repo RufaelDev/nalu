fa68563a8f92989f361790f811b74bdf9fef2cfa
mohammedsraad <mohammedsraad@raadtech.com>
2021-11-16 20:21:38 +0200

    starting point of test nal units

---
+++ tests/test_nal_bytes.py
@@ -0,0 +1,10 @@
+#The test nal units are in the same format as that obtained from pymp4 parser (lists).
+#These nal units are from multiple tested media files in the validator project.
+
+
+sps1_nal = [39,77,64,12,169,24,88,153,128,53,6,1,6,182,222,129,0,47,94,247,192,64]
+sps2_nal =[103, 100, 0, 50, 172, 114, 4, 64, 120, 6, 90, 16, 0, 0, 3, 0, 16, 0, 0, 3, 3, 8, 241, 131, 24, 70]
+sps3_nal = [103, 66, 192, 13, 150, 86, 10, 12, 252, 248, 15, 168, 40, 40, 47, 128, 0, 0, 3, 0, 128, 0, 0, 25, 74]
+pps1_nal = [40, 206, 9, 136]
+pps2_nal = [104, 232, 67, 129, 20, 178, 44]
+pps3_nal = [104, 218, 143, 32]
